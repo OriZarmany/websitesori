376e9f6a952c0bb701d8e0a38844d2e3deaefc40
OriZarmany <orizar16@gmail.com>
2026-03-23 20:25:03 +0200

    עשיתי את הוספת השחקן

---
+++ OriZarmanyWebSite1/addplayers.aspx
@@ -0,0 +1,23 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/MasterPage.master" AutoEventWireup="true" CodeFile="addplayers.aspx.cs" Inherits="addplayers" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" Runat="Server">
+    
+
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" Runat="Server">
+<h2>הוסף שחקן</h2>
+    <label>שם השחקן:</label>
+        <input type="text" name="playerName" /><br />
+    <label> מספר השחקן </label>
+    <input type="number" name="playerNumber" /><br />
+    <label>כמה עונות השחקן משחק במכבי?:</label>
+    <input type="number" name="theplayerssesonsatMaccabi" /><br />
+    <label>גיל השחקן:</label>
+    <input type="number" name="playerage" /><br />
+    <input type="submit" value="הוסף שחקן" />
+
+<br /><br />
+        <strong><%= st %></strong>
+       <%= st %>
+</asp:Content>
+
---
+++ OriZarmanyWebSite1/addplayers.aspx.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Web;
+
+public partial class addplayers : System.Web.UI.Page
+{
+
+    public string st = "";
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (Page.IsPostBack)
+        {
+            string playerName = Request.Form["playerName"];
+            string playerNumber = Request.Form["playerNumber"];
+            string theplayerssesonsatMaccabi = Request.Form["theplayerssesonsatMaccabi"];
+            string playerage = Request.Form["playerage"];
+
+            string sqlInsert =
+                "INSERT INTO PLAYERS (PlayerName, playerNumber, theplayerssesonsatMaccabi, playerage) VALUES (" +
+                "N'" + playerName + "', " +
+                 playerNumber + ", " +
+                  theplayerssesonsatMaccabi + ", " +
+                  playerage + 
+                ")";
+            MyAdoHelper.DoQuery("MyDB.mdf", sqlInsert);
+
+           st = "DONE";
+        }
+
+    }
+}
