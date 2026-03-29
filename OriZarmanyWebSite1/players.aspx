38d421a1fe8fd0c9c6d0b6c93aca4f11990180e6
OriZarmany <orizar16@gmail.com>
2026-03-29 17:37:33 +0300

    הוספתי את הFORM של הplayers

---
+++ OriZarmanyWebSite1/players.aspx
@@ -0,0 +1,21 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="players.aspx.cs" Inherits="players" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Players</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <h2>סגל הקבוצה:</h2>
+
+        <div>
+            <label>שם שחקן:</label>
+            <input type="text" name="playerName" />
+            <input type="submit" value="חפש" />
+        </div>
+        <asp:Literal ID="ltTable" runat="server"></asp:Literal>
+    </form>
+</body>
+</html>
---
+++ OriZarmanyWebSite1/players.aspx.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class players : System.Web.UI.Page
+{
+    public string st = "";
+    protected void Page_Load(object sender, EventArgs e)
+    {
+       //if (!IsPostBack) return;
+
+        string pn = Request.Form["playerName"];
+        string sql = "SELECT * FROM tPlayers WHERE playerName LIKE N'%" + pn + "%'";
+
+        DataTable dt = MyAdoHelper.ExecuteDataTable(sql);
+
+        if (dt.Rows.Count == 0)
+        {
+            st = "אין נתנוים";
+        }
+        else
+        {
+            st += "<table border='1'>";
+            st += "<tr>";
+            st += "<td> playerName </td>";
+            st += "<td> playerNumber </td>";
+            st += "<td> theplayerssesonsatMaccabi </td>";
+            st += "<td> playerage </td>";
+            st += "</tr>";
+
+            for (int i = 0; i < dt.Rows.Count; i++)
+            {
+                st += "<tr>";
+                for (int k = 0; k < dt.Columns.Count; k++)
+                {
+                    st += "<td>" + dt.Rows[i][k] + "</td>";
+                }
+                st += "</tr>";
+            }
+            st += "</table>";
+        }
+        ltTable.Text = st;
+    }
+}
